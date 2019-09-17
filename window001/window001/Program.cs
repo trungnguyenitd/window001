abd1036e0623edb9e46f1b0a14fa2f24b288414d
Nguyễn Văn Trung <trung.nguyen@itd.com.vn>
2019-09-17 09:03:01 +0700

    chuong trinh dau tien

---
+++ window001/window001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace window001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //001
+            Console.Read();
+            //002
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
